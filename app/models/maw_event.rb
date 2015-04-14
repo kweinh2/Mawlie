bb2e00ca025f5f3e7c102bee203afce0ab78bb9e
Kurt Weinheimer <kweinh2@umbc.edu>
2015-04-14 20:01:52 +0000

    heroku csv

--- app/models/maw_event.rb
+++ app/models/maw_event.rb
@@ -1,7 +1,11 @@
 class MawEvent < ActiveRecord::Base
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      MawEvent.create! row.to_hash
+      MawEvent.create({
+        :date => DateTime.strptime(row[0], "%m/%d/%Y"),
+        :where => row[1],
+        :name => row[2]
+        })
     end
   end
 end
