1cd7f991418e85e7c7bf2b7f3af34ddceea80670
Kurt Weinheimer <kweinh2@umbc.edu>
2015-04-14 19:29:59 +0000

    heroku login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,10 @@ Rails.application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+  
+  config.action_mailer.default_url_options = { :host => 'mawlie.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
